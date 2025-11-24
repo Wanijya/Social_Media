46d8575a91bc42bb62fd98a3f663b4f4fa62b12a
Wanijya <wanijyabisen@gmail.com>
2025-11-24 17:26:22 +0530

    adding bcryptjs

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,34 +1,12 @@
+const {
+  registerController,
+  loginController,
+} = require("../controllers/auth.controller");
 const express = require("express");
-const userModel = require("../models/user.model");
-const jwt = require("jsonwebtoken");
 
 const router = express.Router();
 
-/*
-POST /register
-POST /Login
-GET /users [Protected]
-*/
-
-router.post("/register", async (req, res) => {
-  const { username, password } = req.body;
-
-  const existingUser = await userModel.findOne({ username });
-
-  if (existingUser) {
-    return res.status(409).json({ message: "User already exists" });
-  }
-
-  const user = await userModel.create({
-    username,
-    password,
-  });
-
-  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
-
-  res.cookie("token", token);
-
-  res.status(201).json({ message: "User registered successfully", user });
-});
+router.post("/register", registerController);
+router.post("/login", loginController);
 
 module.exports = router;
